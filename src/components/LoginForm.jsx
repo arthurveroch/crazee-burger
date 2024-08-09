1b805c8a2bf61128ca7f8758bd21ec58b2af12d4
arthurveroch <verhofstadtpro@gmail.com>
2024-08-09 11:45:24 +0200

    Résolution de bugs et application de styled components

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../assets/styles/login.css';
+import styled from 'styled-components';
 
 export default function LoginForm() {
   const [name, setName] = useState('');
@@ -17,7 +17,7 @@ export default function LoginForm() {
   };
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <LoginFormStyled onSubmit={handleSubmit}>
         <div className='input-container'>
           <img src='../src/assets/images/icon-name.png' />
           <input
@@ -31,7 +31,61 @@ export default function LoginForm() {
         <button>
           Accédez à votre espace <img src='../src/assets/images/chevron.png' />
         </button>
-      </form>
+      </LoginFormStyled>
     </>
   );
 }
+
+const LoginFormStyled = styled.form`
+  input {
+    all: unset;
+    width: 324px;
+    height: 19px;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  input::placeholder {
+    color: #d3d3d3;
+  }
+
+  .input-container {
+    width: 400px;
+    height: 55px;
+    background: #fff;
+    border-radius: 5px;
+    margin-bottom: 18px;
+    margin-top: 18px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 12.8px;
+    padding: 20px 24px;
+  }
+
+  form {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  button {
+    all: unset;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 15px;
+    font-weight: 700;
+    width: 400px;
+    height: 53px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    background: #ff9f1b;
+    border-radius: 5px;
+  }
+
+  button img {
+    margin-left: 9.8px;
+  }
+`;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,10 @@
 import LoginForm from '../components/LoginForm';
-import '../assets/styles/login.css';
+import styled from 'styled-components';
 
 export default function LoginPage() {
   return (
     <>
-      <div className='container'>
+      <LoginPageStyled>
         <div className='background-overlay'>
           <div className='logo'>
             <h1 className='logo-text'>Crazee</h1>
@@ -18,7 +18,75 @@ export default function LoginPage() {
             <LoginForm />
           </div>
         </div>
-      </div>
+      </LoginPageStyled>
     </>
   );
 }
+
+const LoginPageStyled = styled.div`
+  width: 100%;
+  height: 100svh;
+  background-image: url('../src/assets/images/background-login.jpeg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  overflow: hidden;
+
+  .background-overlay {
+    width: 100%;
+    height: 100svh;
+    background: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    flex-direction: column;
+  }
+
+  .logo {
+    display: flex;
+    gap: 20px;
+  }
+
+  .logo img {
+    width: 200px;
+  }
+
+  .logo-text {
+    font-family: 'Amatic SC', sans-serif;
+    font-weight: 700;
+    font-size: 110px;
+    color: #ff9f1b;
+  }
+
+  .form {
+    width: 464px;
+    height: 438px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .form h1,
+  h2 {
+    color: #fff;
+    font-family: 'Amatic SC', sans-serif;
+  }
+
+  .form h2 {
+    font-size: 36px;
+  }
+
+  .form h1 {
+    font-size: 48px;
+  }
+
+  .line {
+    width: 400px;
+    height: 3px;
+    background-color: #f56a2c;
+    margin-top: 32px;
+    margin-bottom: 40px;
+  }
+`;
